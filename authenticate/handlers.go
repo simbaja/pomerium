7cebc7be349ce6e8f401a18a8c63c0f414e4c6d7
Jack Simbah <jack.simbach@gmail.com>
2020-06-07 00:56:04 -0400

    - disabled cors check

--- authenticate/handlers.go
+++ authenticate/handlers.go
@@ -56,12 +56,12 @@ func (a *Authenticate) Mount(r *mux.Router) {
 	v := r.PathPrefix("/.pomerium").Subrouter()
 	c := cors.New(cors.Options{
 		AllowOriginRequestFunc: func(r *http.Request, _ string) bool {
-			log.Debug().Str("url", r.URL.String()).Str("absUrl", urlutil.GetAbsoluteURL(r).String()).Msg("authenticate: signing request")
-			err := middleware.ValidateRequestURL(r, a.sharedKey)
-			if err != nil {
-				log.FromRequest(r).Info().Err(err).Msg("authenticate: origin blocked")
-			}
-			return err == nil
+			//err := middleware.ValidateRequestURL(r, a.sharedKey)
+			//if err != nil {
+			//	log.FromRequest(r).Info().Err(err).Msg("authenticate: origin blocked")
+			//}
+			//return err == nil
+			return true;
 		},
 		AllowCredentials: true,
 		AllowedHeaders:   []string{"*"},
